55ba06165f223b9e7c909bbba8b0df0a05de624c
KnoxMLG <destroyernv2004@gmail.com>
2023-10-01 15:18:14 -0400

    firebase 2

--- app/src/main/java/com/example/gcc/MainActivity.java
+++ app/src/main/java/com/example/gcc/MainActivity.java
@@ -4,6 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
