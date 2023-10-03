a2535e7dc65ad797d25f5c1d5a699dfbc973799b
KnoxMLG <destroyernv2004@gmail.com>
2023-10-03 19:01:32 -0400

    testing db stuff

--- app/src/main/java/com/example/gcc/MainActivity.java
+++ app/src/main/java/com/example/gcc/MainActivity.java
@@ -10,11 +10,23 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import javax.xml.transform.Result;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public Result register(String email,String password, String role){
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        DatabaseReference newUserRole = db.getReference("users/"+email+"/role");
+        DatabaseReference newUserPassword = db.getReference("users/"+email+"/password");
+
+        newUserRole.setValue(role);
+        newUserPassword.setValue(password);
+        //new stuff l8r
     }
 }
