a2d5c7e9398fc8494e3da518314945ccbb51f8b1
Raman Gupta <gr.gupta.raman@gmail.com>
2023-10-04 16:32:35 -0400

    Add method for validating input fields via regex

--- app/src/main/java/com/example/gcc/MainActivity.java
+++ app/src/main/java/com/example/gcc/MainActivity.java
@@ -14,16 +14,30 @@ import javax.xml.transform.Result;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String EMAIL_REGEX = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\\\.[a-zA-Z0-9-.]+$";
+
+    /*
+    At least one letter.
+    At least one digit.
+    At least one special character (in this case, @, #, $, %, ^, &, +, or =).
+    Minimum length of 8 characters.
+    */
+    private final String PASSWORD_REGEX = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@#$%^&+=!])([A-Za-z\\d@#$%^&+=!]){8,}$";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public Result register(String email,String password, String role){
+    private boolean validateField(String field, String regex) {
+        return field.equals(regex);
+    }
+
+    public Result register(String email, String password, String role) {
         FirebaseDatabase db = FirebaseDatabase.getInstance();
-        DatabaseReference newUserRole = db.getReference("users/"+email+"/role");
-        DatabaseReference newUserPassword = db.getReference("users/"+email+"/password");
+        DatabaseReference newUserRole = db.getReference("users/" + email + "/role");
+        DatabaseReference newUserPassword = db.getReference("users/" + email + "/password");
 
         newUserRole.setValue(role);
         newUserPassword.setValue(password);
