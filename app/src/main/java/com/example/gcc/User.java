2239c5848fa822f624e76975a800f0908016387e
Raman Gupta <gr.gupta.raman@gmail.com>
2023-10-09 15:34:02 -0400

    Add fields & remove methods from User class

--- app/src/main/java/com/example/gcc/User.java
+++ app/src/main/java/com/example/gcc/User.java
@@ -1,21 +1,16 @@
 package com.example.gcc;
 
 public class User extends Account {
-    String[] userInfo;
+    public String password;
+    public String role;
+    public Event[] joinedEvents;
 
-    public void getClubs() throws Exception {
-        throw new Exception("To be implemented");
-    }
-
-    public void getJoinedClubs() throws Exception {
-        throw new Exception("To be implemented");
+    public User(String password, String role) {
+        this.password = password;
+        this.role = role;
     }
 
     public void getJoinedEvents() throws Exception {
-        throw new Exception("To be implemented");
-    }
-
-    public void joinClub() throws Exception {
         throw new Exception("To be implemented");
     }
 
