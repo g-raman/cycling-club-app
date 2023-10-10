dd26d41b527effd389249300f3c9167b4ccfa6c7
Raman Gupta <gr.gupta.raman@gmail.com>
2023-10-10 14:57:54 -0400

    Update constructor for ClubOwner class

--- app/src/main/java/com/example/gcc/ClubOwner.java
+++ app/src/main/java/com/example/gcc/ClubOwner.java
@@ -4,9 +4,17 @@ public class ClubOwner extends Account {
     public String clubName;
     public Event[] hostedEvents;
 
-    public ClubOwner(String clubName, String password) {
-        this.clubName = clubName;
+    public ClubOwner(String clubName, String username, String password, String role) {
+        this.username = username;
         this.password = password;
+        this.role = role;
+        this.clubName = clubName;
+    }
+
+    public ClubOwner(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
     }
 
     public void createEvent() throws Exception {
