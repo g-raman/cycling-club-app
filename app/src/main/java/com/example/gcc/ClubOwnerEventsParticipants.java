f69b7aef0438f23f6209c938bf344ccab64de3f2
pharaohperry <mperr139@uottawa.ca>
2023-11-24 15:33:08 -0500

    Added Club event Participants page

---
+++ app/src/main/java/com/example/gcc/ClubOwnerEventsParticipants.java
@@ -0,0 +1,14 @@
+package com.example.gcc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClubOwnerEventsParticipants extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_club_owner_events_participants);
+    }
+}
